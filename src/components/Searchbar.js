5d875fe911ebd2f8ebf1bff8a5dfe0769dd6f42f
smckenzie78 <109680885+smckenzie78@users.noreply.github.com>
2023-07-21 13:18:22 -0400

    css changes

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -2,7 +2,9 @@ import React from "react";
 import '../styles/Searchbar.css';
 function Searchbar(){
     return(
-        <input type="text" placeholder="Enter in a location..."></input>
+        <div className="search-container">
+            <input type="text" placeholder="Enter in a location..."></input>
+        </div>
     );
 }
 
